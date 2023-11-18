4867d912144b394c5587998af06ad55df3b9cf6b
Venkata Hanuma Teja . Kampa <hanumateja5050@gmail.com>
2023-11-18 20:09:02 +0530

    Delete Landing_Page directory

--- Landing_Page/script.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementsByClassName('toggle')[0];
-const link = document.getElementsByClassName('links')[0];
-
-toggle.addEventListener("click", () =>
-{
-    link.classList.toggle('active');
-});
